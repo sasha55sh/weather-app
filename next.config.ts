87b73c5fa5c5e95898c63e4a9fed396841d750b6
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-09-03 21:43:41 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["randomuser.me"]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
